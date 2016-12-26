05f35bb97668213e8ff1546867bbb89701f053aa
Enjoy <Enjoy>
2016-12-26 16:35:09 +0100

    Initial commit

---
+++ app/src/main/java/enterprise/sample/com/start_service_and_stop_service_with_seconde_class/MainActivity.java
@@ -0,0 +1,33 @@
+package enterprise.sample.com.start_service_and_stop_service_with_seconde_class;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+
+
+
+
+        import android.util.Log;
+        import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    String msg = "Android : ";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+}
